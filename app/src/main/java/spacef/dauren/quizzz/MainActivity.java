e9251f3934c9cd689c22d74baa8ad534b6e1e8d8
Dauren <213053@astanait.edu.kz>
2022-03-24 02:02:09 +0600

    Added difficulty levels in "play menu".

--- app/src/main/java/spacef/dauren/quizzz/MainActivity.java
+++ app/src/main/java/spacef/dauren/quizzz/MainActivity.java
@@ -2,9 +2,12 @@ package spacef.dauren.quizzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_start = (Button)findViewById(R.id.button_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(MainActivity.this, PlayMenu.class);
+                    startActivity(intent);
+                } catch(Exception e) {/* EMPTY */}
+            }
+        });
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
