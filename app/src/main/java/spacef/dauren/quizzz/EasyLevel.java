a248079d8881972e8a8a258fc221acf0f5bbd410
Dauren <213053@astanait.edu.kz>
2022-03-24 23:58:10 +0600

    -Updated EasyLevel.java (assigned onclicklistener to button "1") -Some updates in "Novice Level" activity (added "Back" button, updated system "back" button, added range of success, added two blocks)

--- app/src/main/java/spacef/dauren/quizzz/EasyLevel.java
+++ app/src/main/java/spacef/dauren/quizzz/EasyLevel.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -30,5 +31,21 @@ public class EasyLevel extends AppCompatActivity {
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Button to go to the first level - start
+        TextView tv = (TextView)findViewById(R.id.TextView1);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(EasyLevel.this, EasyLevel1.class);
+                    startActivity(intent);
+                    finish();
+                } catch (Exception e) { /* EMPTY */}
+
+            }
+        });
+        // Button to go to the first level - end
+
     }
 }
--- app/src/main/java/spacef/dauren/quizzz/EasyLevel1.java
+++ app/src/main/java/spacef/dauren/quizzz/EasyLevel1.java
@@ -2,9 +2,14 @@ package spacef.dauren.quizzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class EasyLevel1 extends AppCompatActivity {
 
@@ -13,9 +18,46 @@ public class EasyLevel1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
 
+
+        // Round the corners of the left image - start
+        final ImageView img_left = (ImageView)findViewById(R.id.img_left);
+        img_left.setClipToOutline(true);
+        // Round the corners of the left image - end
+
+        // Round the corners of the right image - start
+        final ImageView img_right = (ImageView)findViewById(R.id.img_right);
+        img_right.setClipToOutline(true);
+        // Round the corners of the right image - end
+
         // Expand the game to fullscreen - start
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         // Expand the game to fullscreen - end
+
+        // Set OnClickListener to button "Back" - start
+        Button button_back = (Button)findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(EasyLevel1.this, EasyLevel.class);
+                    startActivity(intent);
+                    finish();
+                } catch (Exception e) { /* EMPTY */}
+
+            }
+        });
+        // Set OnClickListener to button "Back" - end
     }
+
+    // Set system button "Back" - start
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(EasyLevel1.this, EasyLevel.class);
+        startActivity(intent);
+        finish();
+    }
+
+    // Set system button "Back" - end
 }
